0406b148f1ff873e06471047a6ea69bf0705844f
Xixi Chen <31266066+xixihaha4@users.noreply.github.com>
2018-03-17 16:50:22 -0400

    fix missing brackets in server.js

--- server/server.js
+++ server/server.js
@@ -24,10 +24,14 @@ app.get('/fetch/items', (req, res) => {
     .then((data) => {
       res.send(data);
     });
+});
+
 app.get('/*', (req, res) => {
   res.sendFile(path.join(__dirname, '../client/dist/index.html'), (err) => {
     if (err) res.status(500).send(err);
   });
 });
 
-app.listen(port);
+app.listen(port, function() {
+  console.log(`listening on port ${port}`);
+});
