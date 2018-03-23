ae5b86cca48bf5a940aa4b083676e6fef8239d21
Eric Sin <eric@Erics-MacBook-Pro.local>
2018-03-23 14:17:30 -0400

    added an ALL category

--- client/src/components/saleCategory.jsx
+++ client/src/components/saleCategory.jsx
@@ -2,13 +2,14 @@ import React from 'react';
 
 const SaleCategory = ({ menuCategories, filterByCategory }) => (
   <div >
+        <button type="button" className={`category_0`} onClick={() => filterByCategory()}>All</button>
     {
       menuCategories.map((category, i) =>
-        <button key={i} type="button" className={`category_${i}`} onClick={() => filterByCategory(category)}>{category.category_name}</button>)
+        <button key={i} type="button" className={`category_${i + 1}`} onClick={() => filterByCategory(category)}>{category.category_name}</button>)
     }
     {
       menuCategories.map((category, i) =>
-        <button key={i} type="button" className={`category_${i}`} onClick={() => filterByCategory(category)}>{category.category_name}</button>)
+        <button key={i} type="button" className={`category_${i + 1}`} onClick={() => filterByCategory(category)}>{category.category_name}</button>)
     }
   </div>
 );
