ee7cd9564c1ca0889208144ee7720a0a234eb1ba
Jerry Chen <jieningjchen@gmail.com>
2018-04-06 01:28:23 -0400

    Fixing styling issues with landing page and application

--- client/landingPage/homepage.jsx
+++ client/landingPage/homepage.jsx
@@ -53,25 +53,25 @@ class HomePage extends React.Component {
                 <Meta
                   title="Eric"
                 />
-                <img style={{ marginTop: '5%', width: '70%', height: '70%' }} src="https://avatars3.githubusercontent.com/u/32647671?s=400&u=7b3abb436a9b17e8c2da951015f41967ba58e808&v=4" />
+                <img style={{ marginTop: '5%', marginLeft: '15%', width: '70%', height: '70%' }} src="https://avatars3.githubusercontent.com/u/32647671?s=400&u=7b3abb436a9b17e8c2da951015f41967ba58e808&v=4" />
               </Card.Grid>
               <Card.Grid style={{ width: '25%', height: '40vh'}}>
                 <Meta
                   title="Xixi"
                 />
-                <img style={{ marginTop: '5%', width: '70%', height: '70%' }} src="https://s3.amazonaws.com/tyrell-pos/Fri+Mar+30+2018+19%3A48%3A25+GMT-0400+(EDT)-xixi.jpeg" />
+                <img style={{ marginTop: '5%', marginLeft: '15%', width: '70%', height: '70%' }} src="https://s3.amazonaws.com/tyrell-pos/Fri+Mar+30+2018+19%3A48%3A25+GMT-0400+(EDT)-xixi.jpeg" />
               </Card.Grid>
               <Card.Grid style={{ width: '25%', height: '40vh'}}>
                 <Meta
                   title="Manos"
                 />
-                <img style={{ marginTop: '5%', width: '70%', height: '70%' }} src="https://avatars3.githubusercontent.com/u/32654968?s=460&v=4" />
+                <img style={{ marginTop: '5%', marginLeft: '15%', width: '70%', height: '70%' }} src="https://avatars3.githubusercontent.com/u/32654968?s=460&v=4" />
               </Card.Grid>
               <Card.Grid style={{ width: '25%', height: '40vh' }}>
                 <Meta
                   title="Jerry"
                 />
-                  <img style={{ marginTop: '5%', width: '70%', height: '70%' }} src="https://s3.amazonaws.com/tyrell-pos/Fri+Mar+30+2018+19%3A48%3A25+GMT-0400+(EDT)-jerry.png" />
+                  <img style={{ marginTop: '5%', marginLeft: '15%', width: '70%', height: '70%' }} src="https://s3.amazonaws.com/tyrell-pos/Fri+Mar+30+2018+19%3A48%3A25+GMT-0400+(EDT)-jerry.png" />
               </Card.Grid>
             </Card>
           </div>
